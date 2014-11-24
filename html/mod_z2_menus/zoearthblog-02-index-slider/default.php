426b17405d0490af860ec088b4d31382f5a60e0b
zoearthmoon@gmail.com <zoearthmoon@gmail.com>
2014-11-24 20:59:48 +0800

    修改 slider

--- html/mod_z2_menus/zoearthblog-02-index-slider/default.php
+++ html/mod_z2_menus/zoearthblog-02-index-slider/default.php
@@ -2,35 +2,34 @@
 defined('_JEXEC') or die;
 $menus = $menus[key($menus)]['menus'];
 ?>
-<div class="flexslider">
-    <ul class="slides">
-        <?php if (count($menus) > 0 ):?>
-        
-            <?php foreach ($menus as $p=>$menuC):?>
-            <?php if (Z2HelperImage::exist($menuC['image'])):?>
-            <li>
-                <a href="<?php echo $menuC['link'] ?>">
-                    <img src="<?php echo Z2HelperImage::exist($menuC['image'],770,354)?>" alt="<?php echo $menuC['name'] ?>" />
-                </a>
-                <div class="span12">
-                    <h3 class="flex-caption"><?php echo $menuC['name'] ?></h3>
-                    <p><?php echo Z2HelperUtilities::characterLimit($menuC['description'],300) ?>...</p>
-                </div>
-            </li>
-            <?php endif;?>
-            <?php endforeach;?>
-        
-        <?php endif;?>
-    </ul>
+<?php if (count($menus) > 0 ):?>
+<div id="myCarousel" class="carousel">
+<ol class="carousel-indicators">
+    <?php foreach ($menus as $p=>$menuC):?>
+    <?php if (Z2HelperImage::exist($menuC['image'])):?>
+        <li data-target="#myCarousel" <?php echo $p == 0 ? 'class="active"':''?> data-slide-to="<?php echo $p?>"></li>
+    <?php endif;?>
+    <?php endforeach;?>
+</ol>
+<div class="carousel-inner">
+    <?php foreach ($menus as $p=>$menuC):?>
+    <?php if (Z2HelperImage::exist($menuC['image'])):?>
+        <div class="item <?php echo $p == 0 ? 'active':''?> ">
+            <img src="<?php echo Z2HelperImage::_($menuC['image'],770,354,'F')?>" alt="<?php echo $menuC['name'] ?>" />
+            <div class="carousel-caption">
+            <h4><a href="<?php echo $menuC['link'] ?>"><?php echo $menuC['name'] ?></a></h4>
+            <p><?php echo Z2HelperUtilities::characterLimit($menuC['description'],300) ?>...</p>
+            </div>
+        </div>
+    <?php endif;?>
+    <?php endforeach;?>
 </div>
+<a class="left carousel-control" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+<a class="right carousel-control" href="#myCarousel" data-slide="next">&rsaquo;</a>
+</div>
+<?php endif;?>
 <script type="text/javascript">
-jQuery(window).load(function(){
-    jQuery('.flexslider').flexslider({
-        animation: "slide",
-        slideshow: true,
-        start: function(slider){
-            jQuery('body').removeClass('loading');
-        }
-    });
+jQuery(document).ready(function(){
+    jQuery('#myCarousel').carousel();
 });
 </script>
