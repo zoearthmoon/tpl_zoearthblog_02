d90d346f72a10245742a86d9898e2b717c1ec552
zoearthmoon@gmail.com <zoearthmoon@gmail.com>
2014-11-27 10:20:08 +0800

    修正頁面限制

--- page_inside.php
+++ page_inside.php
@@ -19,6 +19,7 @@ else
     <jdoc:include type="head" />
     <?php echo $favicon;?>
     <?php echo $this->params->get('gacode');?>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
     <!--[if lt IE 9]>
         <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
         <link rel="stylesheet" href="<?php echo $tmpUrl;?>css/style-ie.css"/>
